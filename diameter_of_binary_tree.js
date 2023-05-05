b6301e8bf5b513be667c24a48f106a4e3dcaa923
tracey chung <tchung97@gmail.com>
2023-05-04 20:00:07 -0700

    Diameter of Binary Tree

---
+++ diameter_of_binary_tree.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+var diameterOfBinaryTree = function (root) {
+  if (!root) {
+    return null;
+  }
+  var res = 0;
+
+  function dfs(root) {
+    if (!root) {
+      return 0;
+    }
+
+    let left = dfs(root.left);
+    let right = dfs(root.right);
+    res = Math.max(res, left + right);
+
+    return 1 + Math.max(left, right);
+  }
+
+  dfs(root);
+  return res;
+};
